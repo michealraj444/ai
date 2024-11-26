74909260ed74282f03e5d98a72d6e8cc07d3d203
michealraj444 <reddymr867@gmail.com>
2024-11-26 22:02:26 +0530

    Update a*.py

--- a*.py
+++ a*.py
@@ -1,62 +1,33 @@
 class Graph:
-    def __init__(self, adjacency_list):
-        self.adjacency_list = adjacency_list
-
-    def get_neighbors(self, node):
-        return self.adjacency_list.get(node, [])
-
-    def a_star_algorithm(self, start, goal):
-        open_list = {start}
-        closed_list = set()
-        g = {start: 0}
-        parents = {start: None}
-
+    def __init__(self, adj_list, heuristic):
+        self.adj_list = adj_list
+        self.heuristic = heuristic
+    def a_star(self, start, goal):
+        open_list, g, parents = {start}, {start: 0}, {start: None}
         while open_list:
-            current = min(open_list, key=lambda node: g[node])
-
+            current = min(open_list, key=lambda x: g[x] + self.heuristic.get(x, float('inf')))
             if current == goal:
                 path = []
-                while current is not None:
+                while current:
                     path.append(current)
                     current = parents[current]
                 return path[::-1]
-
             open_list.remove(current)
-            closed_list.add(current)
-
-            for neighbor, weight in self.get_neighbors(current):
-                if neighbor in closed_list:
-                    continue
-                tentative_g = g[current] + weight
-
-                if neighbor not in open_list or tentative_g < g.get(neighbor, float('inf')):
+            for neighbor, weight in self.adj_list.get(current, []):
+                new_cost = g[current] + weight
+                if neighbor not in g or new_cost < g[neighbor]:
                     open_list.add(neighbor)
-                    g[neighbor] = tentative_g
+                    g[neighbor] = new_cost
                     parents[neighbor] = current
-
         return None
-
-
-# Input the graph
-n = int(input("Enter the number of nodes: "))
-adjacency_list = {}
-
-for _ in range(n):
-    node = input("Enter node: ")
-    neighbors = input(f"Enter neighbors of {node} (format: neighbor1,weight1 neighbor2,weight2 ...): ")
-    adjacency_list[node] = [
-        (neighbor.split(",")[0], int(neighbor.split(",")[1]))
-        for neighbor in neighbors.split()
-    ]
-
-start = input("Enter the start node: ")
-goal = input("Enter the goal node: ")
-
-graph = Graph(adjacency_list)
-path = graph.a_star_algorithm(start, goal)
-
-if path:
-    print("Path:", path)
-else:
-    print("No path found.")
-
+# Input
+adj_list = {}
+heuristic = {}
+for _ in range(int(input("Enter number of nodes: "))):
+    node = input("Node: ")
+    adj_list[node] = [(n.split(',')[0], int(n.split(',')[1])) for n in input(f"Neighbors of {node} (neighbor,weight ...): ").split()]
+    heuristic[node] = int(input(f"Heuristic for {node}: "))
+start = input("Start node: ")
+goal = input("Goal node: ")
+graph = Graph(adj_list, heuristic)
+print("Path:", graph.a_star(start, goal) or "No path found")
